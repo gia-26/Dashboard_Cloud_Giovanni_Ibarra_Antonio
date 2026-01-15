9bccdabbe934a6170bf6b62f5a6025cff77a6d97
Giovanni <20240990@uthh.edu.mx>
2026-01-15 15:26:24 -0600

    gio

--- app.js
+++ app.js
@@ -16,15 +16,15 @@ const cargarServicios = () => {
                 <div class='card'>
                     <h3>Servicio: ${servicio.nombre}</h3>
                     <p class='tipo'>Tipo: ${servicio.tipo}</p>
-                    <p>Estado: <span class='activo'>Activo</span></p>
                     <p>Costo: ${servicio.costo}</p>
+                    <p>Estado: <span class='activo'>Activo</span></p>
                 </div>`;
         else
             contenedor.innerHTML += `<div class='card'>
                     <h3>Servicio: ${servicio.nombre}</h3>
                     <p class='tipo'>Tipo: ${servicio.tipo}</p>
-                    <p>Estado: <span class='inactivo'>Inactivo</span></p>
                     <p>Costo: ${servicio.costo}</p>
+                    <p>Estado: <span class='inactivo'>Inactivo</span></p>
                 </div>`;
     })
 }
